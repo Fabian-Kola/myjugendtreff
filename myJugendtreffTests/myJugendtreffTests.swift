f5f4c6021eb7d6483254f574b111232615f5e419
Fabian Kola <fabiankola@yahoo.de>
2015-11-16 14:45:45 +0100

    Initial Commit

---
+++ myJugendtreffTests/myJugendtreffTests.swift
@@ -0,0 +1,36 @@
+//
+//  myJugendtreffTests.swift
+//  myJugendtreffTests
+//
+//  Created by Fabian Kola on 16.11.15.
+//  Copyright © 2015 Fabian Kola. All rights reserved.
+//
+
+import XCTest
+@testable import myJugendtreff
+
+class myJugendtreffTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
